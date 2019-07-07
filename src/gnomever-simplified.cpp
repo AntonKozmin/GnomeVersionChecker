e2f37d91479fe386001faac1e1c8df3d185cfee5
AntonKozmin <48100657+AntonKozmin@users.noreply.github.com>
2019-07-07 12:37:42 +0300

    simplified version
    a simplified version added

---
+++ src/gnomever-simplified.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// A simple version but much too big at the finish 
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main()
+{
+    string GnomeVerFILE=R"(/usr/share/gnome/gnome-version.xml)";
+    ifstream fs(GnomeVerFILE);
+    if (fs.is_open())
+    {
+        string outLine,resLine;
+        regex numregex(R"(>[0-9]\d*<)");
+        smatch sm;
+
+        for (string str; getline(fs, str);) outLine+=str;
+
+        while(regex_search(outLine, sm, numregex))
+        {
+            string ts=sm.str();
+            ts.erase(0,1);
+            ts.pop_back();
+            resLine+=ts;
+            resLine.append(1,'.');
+            outLine=sm.suffix();
+        }
+        resLine.pop_back();
+        cout<<resLine<<endl;
+    }
+    else cout << "?.?.?"<<endl;
+    return 0;
+}
