356976eb3a0352bf70cab47018068d330e18c41d
AntonKozmin <48100657+AntonKozmin@users.noreply.github.com>
2019-07-07 12:33:27 +0300

    Update gnomeversionreader.sh

--- sh/gnomeversionreader.sh
+++ sh/gnomeversionreader.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-GNomeVerFILE="/usr/share/gnome/gnome-version.xml"
-if test -e $GNomeVerFILE; then
+GnomeVerFILE="/usr/share/gnome/gnome-version.xml"
+if test -e $GnomeVerFILE; then
 tmpFILE="/tmp/gnome-version"
-cat $GNomeVerFILE | grep platform | sed s'/[<>]/ /g' | sed "s/\bplatform\b//g" > $tmpFILE
-cat $GNomeVerFILE | grep minor | sed s'/[<>]/ /g' | sed "s/\bminor\b//g" >> $tmpFILE
-cat $GNomeVerFILE | grep micro | sed s'/[<>]/ /g' | sed "s/\bmicro\b//g" >> $tmpFILE
+cat $GnomeVerFILE | grep platform | sed s'/[<>]/ /g' | sed "s/\bplatform\b//g" > $tmpFILE
+cat $GnomeVerFILE | grep minor | sed s'/[<>]/ /g' | sed "s/\bminor\b//g" >> $tmpFILE
+cat $GnomeVerFILE | grep micro | sed s'/[<>]/ /g' | sed "s/\bmicro\b//g" >> $tmpFILE
 sed -i ':a;N;$!ba;s/\n//g' $tmpFILE
 sed -i '/^$/d;s/[[:blank:]]//g' $tmpFILE
 sed -i 's/\//./g' $tmpFILE
